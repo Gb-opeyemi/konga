bdf4791cb7db7b7c936859c3e7ce4ad784a552aa
Opeyemi Gbadamosi <gbadamosiopeyemi99@gmail.com>
2020-04-18 00:01:06 +0100

    push first code commit

--- src/test/java/Login/LoginTests.java
+++ src/test/java/Login/LoginTests.java
@@ -1,8 +1,11 @@
 package Login;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 
 import java.util.concurrent.TimeUnit;
 
@@ -26,6 +29,8 @@ public class LoginTests {
         // make page wait while url fully loads
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
+        Actions actions = new Actions(driver);
+
         //click on login/signup button
         driver.findElement(By.linkText("Login / Signup")).click();
 
@@ -38,7 +43,28 @@ public class LoginTests {
         //Click on Login
         driver.findElement(By.xpath("//button[text() = 'Login']")).click();
 
-        driver.quit();
+        //Wait, so the next operation won't happen so fast
+        //Thread.sleep(5000);
+
+        //Mouseover Phones and Tablet Category
+        WebElement Phone = driver.findElement(By.linkText("Phones and Tablets"));
+        actions.moveToElement(Phone).perform();
+
+        //Select the cover and cases menu
+        driver.findElement(By.xpath("//a[@href='/category/cases-covers-5335']")).click();
+        //Thread.sleep(3000);
+
+        //Enter search item into searchbox
+       //driver.findElement(By.cssSelector("input#jsSearchInput.fe151_2KRxn")).sendKeys("iPhone 6s case");
+        driver.findElement(By.xpath("//input[@id='jsSearchInput']")).sendKeys("iPhone 6s case");
+
+        //Press Enter after typing in search text
+        //WebElement search = driver.findElement(By.cssSelector("input#jsSearchInput.fe151_2KRxn"));
+        //search.sendKeys(Keys.ENTER);
+        WebElement search = driver.findElement(By.xpath("//input[@id='jsSearchInput']"));
+        search.sendKeys(Keys.ENTER);
+
+
     }
 
     public static void main(String args[]) throws InterruptedException {
